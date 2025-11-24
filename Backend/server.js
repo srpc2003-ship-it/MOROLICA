edbe947edbba6ed429d3498e83fe90fa06f39402
srpc2003-ship-it <srpc2003@gmail.com>
2025-11-23 20:32:43 -0600

    bd

--- Backend/server.js
+++ Backend/server.js
@@ -11,16 +11,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// ===== Pool DB =====
+const mysql = require("mysql2/promise");
+
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '123456',
-  database: 'elecciones_morolica',
-  waitForConnections: true,
-  connectionLimit: 10,
-  charset: 'utf8mb4'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
 
 const q = async (sql, params = []) => {
   const [rows] = await pool.query(sql, params);
